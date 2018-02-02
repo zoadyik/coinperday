fb417f1ff94951d8e370b2935852e1bc13b04187
zoadyik <grinzsolo@stephack.com>
2018-02-02 13:32:48 +0700

    a

--- getWallet.php
+++ getWallet.php
@@ -18,57 +18,53 @@ if (!is_null($events['events'])) {
 
             $text_ex = explode(" ", $messages);
 
-            if ($text_ex[0] == "protopool") {
-                $json = (file_get_contents('https://protopool.net/api/wallet?address=' . rawurlencode($text_ex[1])));
-                $json = str_replace('"unsold":', ' "unsold": ""', $json);
-                $obj  = json_decode($json);
-
-                if (empty($obj)) {
-                    $result_text = "no data";
-                } else {
-                    $result_text = "currency : " . $obj->currency . "\nbalance : " . $obj->balance . "\nunpaid : " . $obj->unpaid . "\npaid 24 hr : " . $obj->paid24h . "\ntotal : " . $obj->total;
-                }
-
-                echo $result_text;
-            } elseif ($text_ex[0] == 'phiphi') {
-                $json = (file_get_contents('http://www.phi-phi-pool.net/api/walletEx?address=' . rawurlencode($text_ex[1])));
-                $json = str_replace('"unsold":', ' "unsold": ""', $json);
-                $obj  = json_decode($json);
-
-                if (empty($obj)) {
-                    $result_text = "no data";
-                } else {
-                    $result_text = "currency : " . $obj->currency . "\nbalance : " . $obj->balance . "\nunpaid : " . $obj->unpaid . "\npaid 24 hr : " . $obj->paid24h . "\ntotal : " . $obj->total;
-                }
-            } else {
-                $result_text = "no data\n วิธีใช้ poolName walletAddress";
+            switch ($text_ex[0]) {
+                case "protopool":
+                    $json = (file_get_contents('https://protopool.net/api/wallet?address=' . rawurlencode($text_ex[1])));
+                    break;
+                case "phiphi":
+                    $json = (file_get_contents('http://www.phi-phi-pool.net/api/walletEx?address=' . rawurlencode($text_ex[1])));
+                    break;
+                default:
+                    # code...
+                    break;
             }
-            // Build message to reply back
-            $messages = [
-                'type' => 'text',
-                'text' => $result_text,
-            ];
+            $json = str_replace('"unsold":', ' "unsold": ""', $json);
+            $obj  = json_decode($json);
 
-            // Make a POST Request to Messaging API to reply to sender
-            $url  = 'https://api.line.me/v2/bot/message/reply';
-            $data = [
-                'replyToken' => $replyToken,
-                'messages'   => [$messages],
-            ];
-            $post    = json_encode($data);
-            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+            if (empty($obj)) {
+                $result_text = "no data\n วิธีใช้ poolName walletAddress";
+            } else {
+                $result_text = "currency : " . $obj->currency . "\nbalance : " . $obj->balance . "\nunpaid : " . $obj->unpaid . "\npaid 24 hr : " . $obj->paid24h . "\ntotal : " . $obj->total;
+            }
 
-            $ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            $result = curl_exec($ch);
-            curl_close($ch);
-
-            echo $result . "\r\n";
         }
+        // Build message to reply back
+        $messages = [
+            'type' => 'text',
+            'text' => $result_text,
+        ];
+
+        // Make a POST Request to Messaging API to reply to sender
+        $url  = 'https://api.line.me/v2/bot/message/reply';
+        $data = [
+            'replyToken' => $replyToken,
+            'messages'   => [$messages],
+        ];
+        $post    = json_encode($data);
+        $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        echo $result . "\r\n";
     }
 }
-echo "OK";
+}
+    echo "OK";
